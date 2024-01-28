7405e04392bb11254a6e919d498aaa39a9d86381
Shubham Sinha <shubhamkumarsinha25@gmail.com>
2024-01-29 02:04:22 +0530

    Initial commit

---
+++ src/main/java/com/example/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,25 @@
+package com.example.journalApp.controller;
+
+
+import com.example.journalApp.entity.JournalEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+    private Map<Long, JournalEntity> journalEntities=new HashMap<>();
+    @GetMapping
+    public List<JournalEntity> getAll(){
+        return new ArrayList<>(journalEntities.values());
+    }
+    @PostMapping
+    public boolean createEntery(@RequestBody JournalEntity objentry){
+        journalEntities.put(objentry.getId(),objentry);
+        return true;
+    }
+}
